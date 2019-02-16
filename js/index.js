ad9a848ca97e860109db5fd36dba4a021a559ab4
JuanRa <jrrustarazo@gmail.com>
2019-02-16 17:35:02 +0100

    revisión general de la web

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+window.onscroll = function (){
+  
+  let header = document.getElementById('header');
+  header.classList.add('scroll-sticky');
+
+  if (window.scrollY == 0){
+    header.classList.remove('scroll-sticky');
+  }
+}
+
 $(function(){
   $('[data-toggle="tooltip"]').tooltip()
 })
